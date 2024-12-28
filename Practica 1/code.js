0f7b3d38af8ef8c80b1f324864d7ca3c798d03ba
GDieser <germandieser@gmail.com>
2024-12-27 21:06:57 -0300

    Practica inicial

---
+++ Practica 1/code.js
@@ -0,0 +1,14 @@
+
+for(let index = 0; index < 10; index++)
+    {
+        console.log(index);
+    }
+    
+    function saludar(){
+        //Tipos de variables:
+        //let algo = "";
+        //const algo2 = 2;
+
+        var user = document.getElementById("txtUser").value;
+        alert("Hola " + user);
+    }
