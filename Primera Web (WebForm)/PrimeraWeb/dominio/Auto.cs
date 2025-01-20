f88376cd4a86918cee76e2802ac297193b655be8
GDieser <gigerfoss18@gmail.com>
2025-01-19 21:39:02 -0300

    Prueba de modificacion

--- Primera Web (WebForm)/PrimeraWeb/dominio/Auto.cs
+++ Primera Web (WebForm)/PrimeraWeb/dominio/Auto.cs
@@ -8,5 +8,9 @@ namespace dominio
 {
     public class Auto
     {
+        public string Color { get; set; }
+        public string Modelo { get; set; }
+        public int IdAuto { get; set; }
+        public int Anio { get; set; }
     }
 }
