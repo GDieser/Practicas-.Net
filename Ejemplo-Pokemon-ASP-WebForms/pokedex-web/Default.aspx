0473ff4c6206866075671e0e0111397210c8146f
GDieser <germandieser@gmail.com>
2025-01-30 17:47:26 -0300

    Agregando lista, arreglando errores varios

---
+++ Ejemplo-Pokemon-ASP-WebForms/pokedex-web/Default.aspx
@@ -0,0 +1,7 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="pokedex_web._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+    <h1>Hola!</h1>
+<p>Llegaste al Pokedex Web, tu lugar Pokemon...</p>
+</asp:Content>
+
--- Ejemplo-Pokemon-ASP-WebForms/pokedex-web/Properties/AssemblyInfo.cs
+++ Ejemplo-Pokemon-ASP-WebForms/pokedex-web/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("pokedex_web")]
-[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("a623e7eb-05a5-4cca-86c2-de86143c8c08")]
+[assembly: Guid("121a6460-599f-4640-b3c2-5ddb9d586f19")]
 
 // Version information for an assembly consists of the following four values:
 //
